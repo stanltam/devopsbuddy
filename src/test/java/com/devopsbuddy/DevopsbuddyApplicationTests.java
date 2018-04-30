60e10e4c231643c3ff002543f485d488c39bc5b7
stanltam <32281417+stanltam@users.noreply.github.com>
2018-04-30 19:40:31 +0800

    adding 18n functionality

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,29 @@
 package com.devopsbuddy;
-
+import com.devopsbuddy.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DevopsbuddyApplication.class)
+//@SpringApplicationConfiguration(classes = DevopsbuddyApplication.class)
+@WebAppConfiguration
 public class DevopsbuddyApplicationTests {
 
+    @Autowired
+    private I18NService i18NService;
+
     @Test
-    public void contextLoads() {
+    public void testMessageByLocaleService() throws Exception {
+        String expectedResult = "Bootstrap starter template(By Stanley)";
+        String messageId = "index.main.callout";
+        String actual = i18NService.getMessage(messageId);
+        Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
     }
 
 }
