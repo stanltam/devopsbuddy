d8ce24670ee1dba112e8cb329d346fc8bb0b32ec
stanltam <32281417+stanltam@users.noreply.github.com>
2018-05-01 10:54:01 +0800

    Added Payload Page

---
+++ src/main/java/com/devopsbuddy/web/controllers/PayloadController.java
@@ -0,0 +1,16 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PayloadController {
+
+    private static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+    @RequestMapping("/payloadg")
+    public String payload(){
+        return PayloadController.PAYLOAD_VIEW_NAME;
+    }
+
+}
