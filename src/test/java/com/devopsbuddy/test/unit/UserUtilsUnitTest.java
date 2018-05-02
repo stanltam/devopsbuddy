1b839af392bd010abacaeb2aa43b0c943bd592b3
stanltam <32281417+stanltam@users.noreply.github.com>
2018-05-03 00:09:14 +0800

    Create the front end domain model for basic account'

--- src/test/java/com/devopsbuddy/test/unit/UserUtilsUnitTest.java
+++ src/test/java/com/devopsbuddy/test/unit/UserUtilsUnitTest.java
@@ -1,21 +1,31 @@
 package com.devopsbuddy.test.unit;
 
+import com.devopsbuddy.backend.persistance.domain.backend.User;
 import com.devopsbuddy.utils.UserUtils;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.domain.frontend.BasicAccountPayload;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 import java.util.UUID;
 
+/**
+ * Created by tedonema on 15/04/2016.
+ */
 public class UserUtilsUnitTest {
 
     private MockHttpServletRequest mockHttpServletRequest;
 
+    private PodamFactory podamFactory;
+
     @Before
     public void init() {
         mockHttpServletRequest = new MockHttpServletRequest();
+        podamFactory = new PodamFactoryImpl();
     }
 
     @Test
@@ -35,4 +45,23 @@ public class UserUtilsUnitTest {
 
     }
 
+    @Test
+    public void mapWebUserToDomainUser() {
+
+        BasicAccountPayload webUser = podamFactory.manufacturePojoWithFullData(BasicAccountPayload.class);
+        webUser.setEmail("me@example.com");
+
+        User user = UserUtils.fromWebUserToDomainUser(webUser);
+        Assert.assertNotNull(user);
+
+        Assert.assertEquals(webUser.getUsername(), user.getUsername());
+        Assert.assertEquals(webUser.getPassword(), user.getPassword());
+        Assert.assertEquals(webUser.getFirstName(), user.getFirstName());
+        Assert.assertEquals(webUser.getLastName(), user.getLastName());
+        Assert.assertEquals(webUser.getEmail(), user.getEmail());
+        Assert.assertEquals(webUser.getPhoneNumber(), user.getPhoneNumber());
+        Assert.assertEquals(webUser.getCountry(), user.getCountry());
+        Assert.assertEquals(webUser.getDescription(), user.getDescription());
+
+    }
 }
