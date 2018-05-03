0f1a77cf213cfaf8a679675329950b1114c7fc80
stanltam <32281417+stanltam@users.noreply.github.com>
2018-05-03 21:14:14 +0800

    Added upload image file function

--- src/main/java/com/devopsbuddy/web/controllers/SignupController.java
+++ src/main/java/com/devopsbuddy/web/controllers/SignupController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -69,6 +70,7 @@ public class SignupController {
 
     @RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.POST)
     public String signUpPost(@RequestParam(name = "planId", required = true) int planId,
+                             @RequestParam(name="file",required = false) MultipartFile file,
                              @ModelAttribute(PAYLOAD_MODEL_KEY_NAME) @Valid ProAccountPayload payload,
                              ModelMap model) throws IOException {
 
@@ -108,6 +110,23 @@ public class SignupController {
         // plans and roles
         LOG.debug("Transforming user payload into User domain object");
         User user = UserUtils.fromWebUserToDomainUser(payload);
+
+
+        //Store the profile images on Amazon S3 and stores the URL in the user's record
+        if (file!=null && !file.isEmpty()){
+
+            String profileImageUrl = null;
+            if (profileImageUrl !=null){
+                 user.setProfileImageUrl(profileImageUrl);
+            }else {
+                LOG.warn("There was a problem uploading the profile image to S3. Ther user's profile will be created without the image");
+            }
+
+        }
+
+
+
+
 
         // Sets the Plan and the Roles (depending on the chosen plan)
         LOG.debug("Retrieving plan from the database");
